88d084ef7c17c5b5e7eaeccf4a7fe5e432ece5d0
Emmanuel Pius <emmycode4495@gmail.com>
2024-07-28 22:37:20 +0100

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 import userModel from '../models/UserModel.js'
-import { getDataUri } from '../utils/Features.js';
+import { getDataUri } from '../utils/features.js';
 import cloudinary from 'cloudinary'
 
 // REGISTER CONTROLLER
